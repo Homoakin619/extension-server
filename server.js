e23cd5a4f3b2c05ffd98060f34e5d181e401c138
homoakin619 <homohaykins619@gmail.com>
2023-10-01 11:48:46 +0100

    fixed errors

--- server.js
+++ server.js
@@ -8,19 +8,15 @@ const port = process.env.PORT || 3000;
 
 
 
-app.use(bodyParser.json({ limit: "10mb" }));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "html");
-nunjucks.configure("views", {
-  autoescape: true,
-  express: app,
-});
 
 let videoChunks = "";
+
+
 
 app.post("/api/video", (req, res) => {
   try {
@@ -54,29 +50,22 @@ app.post("/api/assemble", (req, res) => {
 });
 
 
-app.post("/api/upload", upload.single("chunk"), (req, res) => {
-  try {
-    const chunkData = req.file.buffer;
-    videoChunks.push(chunkData);
+// app.post("/api/upload", upload.single("chunk"), (req, res) => {
+//   try {
+//     const chunkData = req.file.buffer;
+//     videoChunks.push(chunkData);
 
-    res.status(200).json({ message: "Chunk received successfully" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
+//     res.status(200).json({ message: "Chunk received successfully" });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// });
 
 
-
-app.use("/api", upload.single("chunk"), (req, res) => {
-  try {
-    const body = req.body;
-    const file = req.file;
-    res.status(200).json({ message: "Video saved successfully" });
-  } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
+app.use('/',(req,res) => {
+    return res.send({'message': "Server is up and running"})
+})
 
 
 
