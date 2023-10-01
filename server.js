46ca3957a80a6422e69809270a800ff674c9e4c8
homoakin619 <homohaykins619@gmail.com>
2023-10-01 12:02:14 +0100

    fixed cors issue

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const fs = require("fs");
-
+const cors = require('cors')
 
 const port = process.env.PORT || 3000;
 
