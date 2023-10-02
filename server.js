a9489d389fbc3c60e85cab8d63cca3f07ad54978
homoakin619 <homohaykins619@gmail.com>
2023-10-02 11:28:33 +0100

    added transcription

--- server.js
+++ server.js
@@ -1,35 +1,37 @@
+
 const express = require("express");
 const app = express();
 const path = require("path");
 const fs = require("fs");
-const cors = require('cors')
+const cors = require("cors");
+const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
+const ffmpeg = require("fluent-ffmpeg");
+ffmpeg.setFfmpegPath(ffmpegPath);
+
+const transcribe = require("./nova");
 
 const port = process.env.PORT || 3000;
-
-
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/uploads",express.static(path.join(__dirname, "uploads")));
-
-app.use(cors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Enable credentials (e.g., cookies, authorization headers)
-  }));
-  
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+// url/uploads/videoname.webm
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
 
 let videoChunks = "";
-
-
 
 app.post("/api/video", (req, res) => {
   try {
     const base64Data = req.body.video;
-    
+
     videoChunks += base64Data;
-    // Respond with a success message
     res.status(200).json({ message: "Chunk Received" });
   } catch (error) {
     console.error(error);
@@ -37,17 +39,15 @@ app.post("/api/video", (req, res) => {
   }
 });
 
-app.post("/api/assemble", (req, res) => {
+app.post("/api/assemble", async (req, res) => {
   try {
-    const title = req.body.title
-    const mergedBuffer = Buffer.from(videoChunks, 'base64');
+    const title = req.body.title;
+    const mergedBuffer = Buffer.from(videoChunks, "base64");
     const filename = `uploaded-${title}-record.webm`;
-
-    const filePath = path.join(__dirname, "uploads", filename); 
-
+    const output = `uploads/uploaded-${title}-record.mp3`;
+    const filePath = path.join(__dirname, "uploads", filename);
     fs.writeFileSync(filePath, mergedBuffer);
-
-    console.log(`Video saved as ${filename}`);
+    await convertVideotoAudio(filename,output);
     res.status(200).json({ message: "Video assembled and processed" });
   } catch (error) {
     console.error(error);
@@ -55,28 +55,35 @@ app.post("/api/assemble", (req, res) => {
   }
 });
 
+app.post("/video/transcription", async (req, res) => {
+  const title = req.body.title;
+  const audioPath = `uploads/uploaded-${title}-record.mp3`;
+  try {
+    if (fs.existsSync(audioPath)) {
+      const transcription = await transcribe(audioPath); 
+      res.status(200).json({ transcription });
+    } else {
+      res.status(200).json({message: "not available"})
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 
+async function convertVideotoAudio(input, output) {
+  ffmpeg(input)
+    .output(output)
+    .noVideo()
+    .format("mp3")
+    .outputOptions("-ab", "192k")
+    .run();
+}
 
-// app.post("/api/upload", upload.single("chunk"), (req, res) => {
-//   try {
-//     const chunkData = req.file.buffer;
-//     videoChunks.push(chunkData);
-
-//     res.status(200).json({ message: "Chunk received successfully" });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: "Internal server error" });
-//   }
-// });
-
-
-app.use('/',(req,res) => {
-    
-    return res.send({'message': "Server is up and running"})
-})
-
-
+app.use("/", (req, res) => {
+  return res.send({ message: "Server is up and running" });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
